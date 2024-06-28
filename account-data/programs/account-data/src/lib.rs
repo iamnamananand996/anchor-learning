0d0fe44e48fe405028aa39949807d0f004aa4b8a
namananand3 <iamnamananand996@gmail.com>
2024-06-28 16:48:47 +0530

    Update lib.rs

--- account-data/programs/account-data/src/lib.rs
+++ account-data/programs/account-data/src/lib.rs
@@ -6,10 +6,40 @@ declare_id!("5TJkgRqurg8BjL7SB4dnU7Wd5dSHLurvYNdKXHHo14tA");
 pub mod account_data {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(
+        ctx: Context<CreateAddressInfo>,
+        name: String,
+        house_number: u8,
+        street: String,
+        city: String,
+    ) -> Result<()> {
+        *ctx.accounts.address_info = AddressInfo {
+            name,
+            house_number,
+            street,
+            city,
+        };
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateAddressInfo<'info> {
+    #[account(mut)]
+    pub payer: Signer<'info>,
+    #[account(init, payer = payer, space = 8 + 4 + 50 + 1 + 4 + 50 + 4 + 50)]
+    pub address_info: Account<'info, AddressInfo>,
+    pub system_program: Program<'info, System>,
+}
+
+#[account]
+#[derive(InitSpace)]
+pub struct AddressInfo {
+    #[max_len(50)]
+    pub name: String,
+    pub house_number: u8,
+    #[max_len(50)]
+    pub street: String,
+    #[max_len(50)]
+    pub city: String,
+}
